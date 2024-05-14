2712bbe7d549da04620caf940182c37f5912a480
Corey Buchan <xouwan021592@gmail.com>
2024-05-14 14:59:49 -0700

    Add regions to index so we can collapse and organize more effectively; add styles static path;

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 /** main entry point of the application. */
 
-/** required modules import */
+/** #region requiredModules */
 require("./utils.js");
 require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const bcrypt = require("bcrypt");
+
+/** #region requiredModules */
+
 const saltRounds = 12;
 
 const port = process.env.PORT || 4000;
@@ -17,7 +20,7 @@ const Joi = require("joi");
 
 const expireTime = 1 * 60 * 60 * 1000; //expires after 1 HOUR
 
-/* secret information section */
+/* #region secrets */
 const mongodb_host = process.env.MONGODB_HOST;
 const mongodb_user = process.env.MONGODB_USER;
 const mongodb_password = process.env.MONGODB_PASSWORD;
@@ -25,7 +28,7 @@ const mongodb_database = process.env.MONGODB_DATABASE;
 const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
 
 const node_session_secret = process.env.NODE_SESSION_SECRET;
-/* end secret section */
+/* #endregion secrets */
 
 var { database } = include("databaseConnection");
 
@@ -36,7 +39,7 @@ var { database } = include("databaseConnection");
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 
-/** creates a mondodb store for session data*/
+/* creates a mondodb store for session data*/
 var mongoStore = MongoStore.create({
   mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/sessions`,
   crypto: {
@@ -44,7 +47,7 @@ var mongoStore = MongoStore.create({
   },
 });
 
-/** middleware, handles session management */
+/* #region middleware */
 app.use(
   session({
     secret: node_session_secret,
@@ -53,8 +56,15 @@ app.use(
     resave: true,
   })
 );
-app.use(express.static(__dirname + "/public"));
 
+/* #endregion middleware */
+
+/* #region expressPathing */
+app.use(express.static(__dirname + "/public"));
+app.use("/styles", express.static("./styles"));
+/* #endregion expressPathing */
+
+/* #region serverRouting */
 app.get("/", (req, res) => {
   res.render("index", {});
 });
@@ -78,6 +88,7 @@ app.get("/signup", (req, res) => {
 app.get("/logout", (req, res) => {
   res.render("logout", {});
 });
+
 /**
  * handles all routes that are not matched by any other route.
  * renders a 404 page and sets the response status to 404.
@@ -86,6 +97,8 @@ app.get("*", (req, res) => {
   res.status(404);
   res.render("404");
 });
+
+/* #endregion serverRouting */
 
 /** starts the server and listens on the specified port */
 app.listen(port, () => {
