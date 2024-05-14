30a5889d819c8da51873521cc901cabbaebb1d1a
Polina Omelyantseva <pomelyantseva@my.bcit.ca>
2024-05-14 10:03:26 -0700

    added index.js with some initial functions

---
+++ index.js
@@ -0,0 +1,59 @@
+
+require("./utils.js");
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const bcrypt = require('bcrypt');
+const saltRounds = 12;
+
+const port = process.env.PORT || 3001;
+
+const app = express();
+
+const Joi = require("joi");
+
+const expireTime = 1 * 60 * 60 * 1000; //expires after 1 HOUR  (hours * minutes * seconds * millis)
+
+/* secret information section */
+const mongodb_host = process.env.MONGODB_HOST;
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_database = process.env.MONGODB_DATABASE;
+const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
+
+const node_session_secret = process.env.NODE_SESSION_SECRET;
+/* END secret section */
+
+var {database} = include('databaseConnection');
+
+//const userCollection = database.db(mongodb_database).collection('users');
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: false}));
+
+var mongoStore = MongoStore.create({
+	mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/sessions`,
+	crypto: {
+		secret: mongodb_session_secret
+	}
+})
+
+app.use(session({ 
+    secret: node_session_secret,
+	store: mongoStore, //default is memory store 
+	saveUninitialized: false, 
+	resave: true
+}
+));
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("*", (req,res) => {
+	res.status(404);
+	res.render("404");
+})
+
+app.listen(port, () => {
+	console.log("Node application listening on port "+port);
+}); 
