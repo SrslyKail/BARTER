35bb0b8ea0e892563effd8ab9e5832ad307394dc
Corey Buchan <Xouwan021592@Gmail.com>
2024-05-23 17:09:59 -0700

    add a googleMaps module, update Location with some test queries

--- scripts/modules/location.js
+++ scripts/modules/location.js
@@ -1,27 +1,60 @@
+require("dotenv").config();
+const {
+    Client,
+    PlaceInputType,
+    Language,
+} = require("@googlemaps/google-maps-services-js");
+const client = new Client();
+const GMAPS_API_KEY = process.env.GMAPS_API_KEY;
+console.log(GMAPS_API_KEY);
+
+async function testFetch() {
+    // let res = await client.elevation({
+    //     params: {
+    //         locations: [{ lat: 45, lng: -110 }],
+    //         key: GMAPS_API_KEY,
+    //     },
+    //     timeout: 1000, // milliseconds
+    // });
+    let query = "BCIT Burnaby";
+    // console.log(query);
+    let res = await client.textSearch({
+        params: {
+            language: Language.en,
+            key: GMAPS_API_KEY,
+            input: query,
+            inputtype: PlaceInputType.textQuery,
+        },
+    });
+    // query = query.replaceAll(" ", "+");
+    console.log(res.data);
+}
 /**@type {GeolocationPosition} */
-var userPosition;
+let userPosition;
 
 /** @type {PositionOptions} */
 const positionOptions = {
-  enableHighAccuracy: true,
+    enableHighAccuracy: true,
 };
 
 /**
  * Starts tracking the user location, and sets a watcher for position updates.
  */
 function setupLocation() {
-  //CB: Useful information on how Geolocation works:
-  //https://w3c.github.io/geolocation-api/#dom-geolocationposition
+    //CB: Useful information on how Geolocation works:
+    //https://w3c.github.io/geolocation-api/#dom-geolocationposition
 
-  //uses navigator to get the geolocation
-  //Watch position does it constantly instead of just once
-  navigator.geolocation.watchPosition(
-    (position) => {
-      userPosition = position;
-    },
-    null,
-    positionOptions
-  );
+    //uses navigator to get the geolocation
+    //Watch position does it constantly instead of just once
+    navigator.geolocation.watchPosition(
+        (position) => {
+            userPosition = position;
+        },
+        null,
+        positionOptions
+    );
 }
 
-module.exports = { userPosition, setupLocation };
+setupLocation();
+
+module.exports = { userPosition };
