5ca1de960c49e9bccd2e746c25d5ea8de4dc3fdb
Ben Henry <bhenry18@my.bcit.ca>
2024-05-27 22:57:12 -0700

    clicking specific circles on pill pagination now functions

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,22 +1,22 @@
 
 let currentPage = 0;
-const maxPage = (document.getElementsByClassName("pillGrid")[0].children.length + 1)%3;
+const maxPage = (document.getElementsByClassName("pillGrid")[0].children.length + 1) % 3;
 const left = document.getElementById("leftPag");
 const right = document.getElementById("rightPag");
 let goto
 let actCirc = "circle0"
 let oldCirc
 
-async function leftButton() {
+function leftButton() {
 
-    oldCirc = "circle" + currentPage
+    actCirc = "circle" + currentPage
     currentPage -= 1;
     document.getElementById(actCirc).classList.toggle("activeCircle");
     actCirc = "circle" + currentPage
     document.getElementById(actCirc).classList.toggle("activeCircle");
     goto = "pill" + (currentPage * 3)
     document.getElementById(goto).scrollIntoView()
-    setListeners()
+    setArrowListeners()
 }
 
 function rightButton() {
@@ -27,12 +27,11 @@ function rightButton() {
     document.getElementById(actCirc).classList.toggle("activeCircle");
     goto = "pill" + (currentPage * 3)
     document.getElementById(goto).scrollIntoView()
-    console.log(currentPage)
-    setListeners()
+    setArrowListeners()
 }
 
-async function setListeners() {
-    
+function setArrowListeners() {
+
     left.removeEventListener("click", leftButton);
     right.removeEventListener("click", rightButton);
 
@@ -40,10 +39,33 @@ async function setListeners() {
         left.addEventListener("click", leftButton)
     }
 
-    if (currentPage < maxPage -1 ) {
-        console.log(maxPage)
-        console.log("testing max")
+    if (currentPage < maxPage - 1) {
         right.addEventListener("click", rightButton)
     }
 }
-setListeners()
+
+function scrollTo(circNum) {
+    document.getElementById(actCirc).classList.toggle("activeCircle");
+    document.getElementById("pill" + circNum.currentTarget.myParam).scrollIntoView()
+    actCirc = ("circle" + (circNum.currentTarget.myParam / 3))
+    document.getElementById(actCirc).classList.toggle("activeCircle");
+    currentPage = (circNum.currentTarget.myParam / 3)
+    setArrowListeners()
+}
+
+function setCircleListeners() {
+
+    for (n = 0; n < maxPage; n++) {
+        let curCircle = document.getElementById("circle" + (n))
+        curCircle.myParam = (n * 3)
+        curCircle.addEventListener("click", scrollTo)
+    }
+}
+
+
+
+
+document.getElementById("pill0").scrollIntoView()
+
+setArrowListeners()
+setCircleListeners()
