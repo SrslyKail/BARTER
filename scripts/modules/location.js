4e6fc932e54ee27b87fc0e4a1ce4e848048e8e75
Corey Buchan <Xouwan021592@Gmail.com>
2024-05-29 10:26:53 -0700

    removed functions that arent needed

--- scripts/modules/location.js
+++ scripts/modules/location.js
@@ -41,24 +41,5 @@ const positionOptions = {
     enableHighAccuracy: true,
 };
 
-/**
- * Starts tracking the user location, and sets a watcher for position updates.
- */
-function setupLocation() {
-    //CB: Useful information on how Geolocation works:
-    //https://w3c.github.io/geolocation-api/#dom-geolocationposition
 
-    //uses navigator to get the geolocation
-    //Watch position does it constantly instead of just once
-    navigator.geolocation.watchPosition(
-        (position) => {
-            userPosition = position;
-        },
-        null,
-        positionOptions
-    );
-}
-
-setupLocation();
-
-module.exports = { userPosition };
+module.exports = {  };
