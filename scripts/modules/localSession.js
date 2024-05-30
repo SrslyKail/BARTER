8b5579d0a58826bda6eaae9bc9a33194b1c74905
Corey Buchan <Xouwan021592@Gmail.com>
2024-05-30 12:46:34 -0700

    Refresh cookie for current user on page change. This will reduce the amount they need to log in, and stop those pesky bugs that happen when a user attemps to access a page they USE to have access to, but suddenly dont.

--- scripts/modules/localSession.js
+++ scripts/modules/localSession.js
@@ -73,9 +73,21 @@ function createSession(
  */
 function isAuthenticated(req) {
   let user = getUser(req);
+
   //ternary ensures we always get a boolean output
   //otherwise we might return null
   return user ? user.isAuthenticated : false;
+}
+
+function refreshCookieTime(req) {
+  console.log("Before updating:", req.session.cookie.maxAge);
+  req.session.cookie.maxAge = isAuthenticated(req)
+    ? (req.session.cookie.maxAge += expireTime - req.session.cookie.maxAge)
+    : null;
+  req.session.cookie.maxAge > expireTime
+    ? (req.session.cookie.maxAge = expireTime)
+    : null;
+  console.log("after:", req.session.cookie.maxAge);
 }
 
 /**
@@ -177,4 +189,5 @@ module.exports = {
   getUserId,
   defaultIcon,
   formatProfileIconPath,
+  refreshCookieTime
 };
