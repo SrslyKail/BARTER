e21ad85a25973af503da33cd2d0fb9f143ee6527
Corey Buchan <Xouwan021592@Gmail.com>
2024-06-06 20:20:17 -0700

    Initial commits for moving userCard and Joi Validation Schemas to their own modules; will fill them out later.

---
+++ scripts/modules/userCard.js
@@ -0,0 +1,25 @@
+/** All the arguments the userCard needs */
+class userCard {
+  constructor(
+    username,
+    userSkills,
+    email,
+    userIcon,
+    userLocation = null,
+    rateValue = null,
+    rateCount = null
+  ) {
+    this.username = username;
+    this.userSkills = userSkills;
+    this.email = email;
+    this.userIcon = formatProfileIconPath(userIcon);
+    this.userLocation = userLocation;
+    this.rateValue = rateValue;
+    this.rateCount = rateCount;
+  }
+}
+
+function createUserCard(mongoDocument) {
+}
+
+function createUserCard([...mongoDocuments]) {}
---
+++ scripts/modules/validationSchemas.js
@@ -0,0 +1,22 @@
+const Joi = require("joi");
+
+const ratingSchema = Joi.number().min(1).max(5).required();
+const passwordSchema = Joi.string().max(20).required();
+
+// TODO: CB: Does Joi have inheritence? Can we extend passwordSchema so userSchema inherits from it?
+const userSchema = Joi.object({
+  username: Joi.string().alphanum().max(20).required(),
+  password: Joi.string().max(20).required(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net", "ca"] },
+    })
+    .required(),
+});
+
+const objectIdSchema = Joi.object({
+  objID: Joi.string().hex().length(24),
+});
+
+module.exports(ratingSchema, userSchema, passwordSchema);
