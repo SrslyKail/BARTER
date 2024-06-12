a163ddfe4125fe4c51a728b31ebf40acf5d28112
Corey Buchan <Xouwan021592@Gmail.com>
2024-06-12 13:28:26 -0700

    Refactored userSchema into component parts so we can check each part individually when needed

--- scripts/modules/validationSchemas.js
+++ scripts/modules/validationSchemas.js
@@ -1,24 +1,30 @@
 const Joi = require("joi");
 
 const ratingSchema = Joi.number().min(1).max(5).required();
+const usernameSchema = Joi.string().alphanum().max(20).required();
+const emailSchema = Joi.string()
+  .email({
+    minDomainSegments: 2,
+    tlds: { allow: ["com", "net", "ca"] },
+  })
+  .required();
 const passwordSchema = Joi.string().max(20).required();
 
-// TODO: CB: Does Joi have inheritence? Can we extend passwordSchema so userSchema inherits from it?
-const userSchema = Joi.object({
-  username: Joi.string().alphanum().max(20).required(),
-
-  password: Joi.string().max(20).required(),
-
-  email: Joi.string()
-    .email({
-      minDomainSegments: 2,
-      tlds: { allow: ["com", "net", "ca"] },
-    })
-    .required(),
+const userSchema = Joi.object().keys({
+  username: usernameSchema,
+  password: passwordSchema,
+  email: emailSchema,
 });
 
 const objectIdSchema = Joi.object({
   objId: Joi.string().hex({ prefix: false }).length(24).required(),
 });
 
-module.exports = { ratingSchema, userSchema, passwordSchema, objectIdSchema };
+module.exports = {
+  ratingSchema,
+  usernameSchema,
+  emailSchema,
+  passwordSchema,
+  userSchema,
+  objectIdSchema,
+};
